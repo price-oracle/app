46e12408e752fab482abcd760bc9a31b2d867651
turtlemoji <turtlemoji@protonmail.com>
2022-11-01 08:56:32 -0300

    build: more dependencies and polyfills

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const TestClick = () => {
+    console.log("test");
+  }
+
   return (
     <div className="App">
       <div>
@@ -15,18 +19,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <button onClick={TestClick}>Test</button>
     </div>
   )
 }
