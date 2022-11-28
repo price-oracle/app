243270a0dcab470e71eac42d870d4c14f3a50245
Ardy <ardy@defi.sucks>
2022-11-28 08:52:42 -0400

    chore: move multicall address to a constant

--- src/services/multicallService.ts
+++ src/services/multicallService.ts
@@ -1,13 +1,17 @@
 import { Provider, setMulticallAddress } from 'ethers-multicall';
-import { useProvider } from 'wagmi';
+import { useProvider, useNetwork } from 'wagmi';
+
+import { getConfig } from '~/config';
 
 export class MultiCallService {
   provider = useProvider();
+  multicallAddress = getConfig().ADDRESSES.MULTICALL_ADDRESS;
+  chainId = useNetwork().chain?.id;
   multicallProvider = new Provider(this.provider);
 
   constructor() {
     // Set mainnet multicall address to localhost chain as we are forking
-    setMulticallAddress(1337, '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441');
+    setMulticallAddress(this.chainId || 1337, this.multicallAddress);
   }
 
   async multicall(txs: any[]) {
