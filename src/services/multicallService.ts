3c4e72c3fde183a592bc54b99376152b1ef1bd3b
Ardy <ardy@defi.sucks>
2022-11-28 15:08:14 -0400

    hardcode network id

--- src/services/multicallService.ts
+++ src/services/multicallService.ts
@@ -1,17 +1,16 @@
 import { Provider, setMulticallAddress } from 'ethers-multicall';
-import { useProvider, useNetwork } from 'wagmi';
+import { useProvider } from 'wagmi';
 
 import { getConfig } from '~/config';
 
 export class MultiCallService {
   provider = useProvider();
   multicallAddress = getConfig().ADDRESSES.MULTICALL_ADDRESS;
-  chainId = useNetwork().chain?.id;
   multicallProvider = new Provider(this.provider);
 
   constructor() {
     // Set mainnet multicall address to localhost chain as we are forking
-    setMulticallAddress(this.chainId || 1337, this.multicallAddress);
+    setMulticallAddress(1337, this.multicallAddress);
   }
 
   async multicall(txs: any[]) {
